cea510abad4a88168191efa155f91961f80c9a95
Andy Kay <andykay.me@gmail.com>
2019-01-01 23:40:21 -0500

    changes

--- www/pages/_app.tsx
+++ www/pages/_app.tsx
@@ -1,17 +1,23 @@
-import App, { Container } from 'next/app'
-import React from 'react'
-import withApolloClient from '../lib/with-apollo-client'
-import { ApolloProvider } from 'react-apollo'
+import App, { Container } from "next/app";
+import React from "react";
+import withApolloClient from "../lib/with-apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { ThemeProvider } from "styled-components";
+import theme from "../theme";
 
 class MyApp extends App {
-    render() {
-        const { Component, pageProps, apolloClient } = this.props
-        return <Container>
-            <ApolloProvider client={apolloClient}>
-                <Component {...pageProps} />
-            </ApolloProvider>
-        </Container>
-    }
+  render() {
+    const { Component, pageProps, apolloClient } = this.props;
+    return (
+      <Container>
+        <ApolloProvider client={apolloClient}>
+          <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </ApolloProvider>
+      </Container>
+    );
+  }
 }
 
-export default withApolloClient(MyApp)
+export default withApolloClient(MyApp);
--- www/pages/users.tsx
+++ www/pages/users.tsx
@@ -1,42 +1,83 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import gql from "graphql-tag";
-import { Query } from 'react-apollo';
-import ErrorMessage from '../components/ErrorMessage';
+import { Query } from "react-apollo";
+import ErrorMessage from "../components/ErrorMessage";
+import { Flex, Box, Heading, Card, Image, Text } from "rebass";
 
 interface Props {
-    username: string;
+  username: string;
 }
 
 const GET_USER = gql`
-    query user($username: String!) {
-        user(where: {username: $username} ) {
-            name
-            username
-        }
+  query user($username: String!) {
+    user(where: { username: $username }) {
+      name
+      username
+      photo
     }
+  }
 `;
 
 export default class Users extends Component<Props> {
-    static async getInitialProps({ query }) {
-        return query;
-    }
+  static async getInitialProps({ query }) {
+    return query;
+  }
 
-    render() {
-        return (
-            <Query query={GET_USER} variables={{ username: this.props.username }}>
-                {({ loading, error, data: { user } }) => {
-                    if (loading) return <div>Loading...</div>;
-                    if (error) return <ErrorMessage message={error.message} />;
+  render() {
+    return (
+      <Query query={GET_USER} variables={{ username: this.props.username }}>
+        {({ loading, error, data: { user } }) => {
+          if (loading) return <div>Loading...</div>;
+          if (error) return <ErrorMessage message={error.message} />;
 
-                    return (
-                        <div>
-                            <h1>{user.name}</h1>
-                            <h3>{user.username}</h3>
-                        </div>
-                    );
-                }}
-            </Query>
-        )
-    }
+          return (
+            <Flex justifyContent="center">
+              <Box my={5} css={{ textAlign: "center" }}>
+                <Image
+                  src={user.photo}
+                  alt={user.name}
+                  width={100}
+                  borderRadius={50}
+                  mb={3}
+                />
+                <Heading>{user.username}</Heading>
+
+                <Card
+                  my={2}
+                  p={2}
+                  borderRadius={8}
+                  boxShadow="0 2px 16px rgba(0, 0, 0, 0.25)"
+                >
+                  <Text>Bio</Text>
+                  <p>Dad. Deadlifter. Lover of life.</p>
+                </Card>
+
+                <Box my={2}>
+                  <ul>
+                    <li>
+                      <a href="#">Maker</a>
+                    </li>
+                    <li>
+                      <a href="#">Lifter</a>
+                    </li>
+                  </ul>
+                </Box>
+
+                <Box my={2}>
+                  <iframe
+                    src="https://open.spotify.com/embed/track/1FRFgyPNuqfXIRYlCJ1kwR"
+                    width="300"
+                    height="80"
+                    frameBorder="0"
+                    allowtransparency="true"
+                    allow="encrypted-media"
+                  />
+                </Box>
+              </Box>
+            </Flex>
+          );
+        }}
+      </Query>
+    );
+  }
 }
